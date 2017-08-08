602a0134b0501602ad864b9dd01383672db475b9
Jeff Carlow <jeff.carlow@gmail.com>
2017-08-07 19:37:21 -0600

    almost done. Need logic to select best friend

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+var friends = [
+	{
+		name: "Jeff",
+		photo: "https://pixel.nymag.com/imgs/daily/grub/2014/10/16/16-sean-brock.w1200.h630.jpg",
+		scores: ["5", "5", "4", "4", "3", "4", "3", "2", "3", "4"]
+	},
+  {
+    name: "Joe",
+    photo: "https://pbs.twimg.com/profile_images/786767617742737408/swbHq0cT.jpg",
+    scores: ["5", "5", "4", "4", "3", "4", "3", "2", "3", "4"]
+  }];
+
+module.exports = friends;
