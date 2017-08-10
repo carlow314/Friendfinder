c4aed561bb73e4e7d92c478134f96c8410b2505a
Jeff Carlow <jeff.carlow@gmail.com>
2017-08-09 19:55:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
